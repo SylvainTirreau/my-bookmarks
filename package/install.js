05464e989f3f408b9bb708844b4c45566a0e7885
Sylvain Tirreau <sylvain@preferendum.fr>
2023-03-12 19:25:59 +0100

    Add tsconfig files to watch package and to watch app. First steps to install script.

---
+++ package/install.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+function createFolders() {
+    const path = 'dist/data/links';
+    try {
+        (0, fs_1.mkdirSync)(path, { recursive: true });
+    }
+    catch (err) {
+        console.log(`Cannot create ${path}. Error: ${err.message}`);
+    }
+}
+function initFiles() {
+    const filesPaths = ['dist/data/labels.json', 'dist/data/links.json'];
+    for (const filePath of filesPaths) {
+        try {
+            (0, fs_1.appendFileSync)(filePath, '{}');
+        }
+        catch (err) {
+            console.log(`Cannot create ${filePath}. Error: ${err.message}`);
+        }
+    }
+}
+if (typeof require !== 'undefined' && require.main === module) {
+    createFolders();
+    initFiles();
+}
---
+++ package/install.ts
@@ -0,0 +1,27 @@
+import {mkdirSync, appendFileSync} from "fs"
+
+function createFolders() {
+  const path = 'dist/data/links'
+  try {
+    mkdirSync(path, {recursive: true})
+  } catch (err: any) {
+    console.log(`Cannot create ${path}. Error: ${err.message}`)
+  }
+
+}
+
+function initFiles() {
+  const filesPaths = ['dist/data/labels.json', 'dist/data/links.json']
+  for (const filePath of filesPaths) {
+    try {
+      appendFileSync(filePath, '{}',)
+    } catch (err: any) {
+      console.log(`Cannot create ${filePath}. Error: ${err.message!}`)
+    }
+  }
+}
+
+if (typeof require !== 'undefined' && require.main === module) {
+  createFolders()
+  initFiles()
+}
