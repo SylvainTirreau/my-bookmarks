e890fdcaaf11719e2fc30054eb2ad0d9090fd565
Sylvain Tirreau <sylvain@preferendum.fr>
2023-03-13 16:12:36 +0100

    Server strcture is ok.

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,24 @@ import nunjucks from 'nunjucks'
 import { join } from 'path'
 
 const app: Express = express()
-const port = 3010
+const port = 3005
 
 app.use(express.static(join('dist', 'assets')))
 app.set('view engine', 'html')
 nunjucks.configure(join('dist', 'assets', 'html'), {autoescape: true, express: app, watch: true})
 
+app.get('/', ((req: Request, res: Response) => {
+  console.log()
+}))
+
+app.get('/:labels', ((req: Request, res: Response) => {
+  console.log(req.params.labels)
+}))
+
+app.use((req: Request, res: Response) => {
+  //res.status(404).render('error.html', { code: '404', text: 'Cette URL ne mène nulle part' })
+})
+
+app.listen(port, () => {
+  console.log(`My bookmarks is running on port ${port}...`)
+})
